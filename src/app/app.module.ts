fe85587aadeedd7a87524fc1be9eacb68bb8b777
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 21:21:57 -0300

    Criando componente lista-empregados + criando tabela de empregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEmpregadosComponent } from './lista-empregados/lista-empregados.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpregadosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lista-empregados/lista-empregados.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-lista-empregados',
+  templateUrl: './lista-empregados.component.html',
+  styleUrl: './lista-empregados.component.css'
+})
+export class ListaEmpregadosComponent{
+
+}
