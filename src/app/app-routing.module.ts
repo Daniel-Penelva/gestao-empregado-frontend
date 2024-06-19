e5009d99a9bac182ca6eb1e77977c233aa4c95e1
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 23:21:38 -0300

    Configurando a rota do componente lista de empregados + criando uma navbar (menu de navegação) html para utilizar routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpregadosComponent } from './lista-empregados/lista-empregados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'empregados', component: ListaEmpregadosComponent},
+  {path: '', redirectTo: 'empregados', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
