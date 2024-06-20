f4999cfd99183360bd5d3b8e60409b086aab582f
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-20 20:17:28 -0300

    criando método buscarEmpregadoPorId utilizando subscribe

--- src/app/buscar-empregado/buscar-empregado.component.ts
+++ src/app/buscar-empregado/buscar-empregado.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmpregadoService } from '../service/empregado.service';
+import { Empregado } from '../model/empregado';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-buscar-empregado',
   templateUrl: './buscar-empregado.component.html',
   styleUrl: './buscar-empregado.component.css'
 })
-export class BuscarEmpregadoComponent {
+export class BuscarEmpregadoComponent implements OnInit{
+
+  id: number;
+  empregado: Empregado;
+
+  constructor(private route: ActivatedRoute, private empregadoService: EmpregadoService){}
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.params['id'];
+      this.empregado = new Empregado();
+      this.empregadoService.buscarEmpregadoPorId(this.id).subscribe(date => {
+        this.empregado = date;
+        alert(`Detalhes do empregado ${this.empregado.nome}`);
+      });
+  }
 
 }
