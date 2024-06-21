9c5da5a03fac2dc869467dbd33755cca3a68581e
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-20 21:00:30 -0300

    criando a rota de buscar empregado (por id)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaEmpregadosComponent } from './lista-empregados/lista-empregados.component';
 import { CadastrarEmpregadoComponent } from './cadastrar-empregado/cadastrar-empregado.component';
+import { BuscarEmpregadoComponent } from './buscar-empregado/buscar-empregado.component';
 
 const routes: Routes = [
   {path: 'empregados', component: ListaEmpregadosComponent},
   {path: '', redirectTo: 'empregados', pathMatch: 'full'},
-  {path: 'cadastrar-empregado', component: CadastrarEmpregadoComponent}
+  {path: 'cadastrar-empregado', component: CadastrarEmpregadoComponent},
+  {path: 'buscar-empregado-id/:id', component: BuscarEmpregadoComponent}
 ];
 
 @NgModule({
