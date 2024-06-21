96e33c2ee870beb4fd8ada7a5d9b8f1051c09260
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-21 01:25:51 -0300

    criando a rota de atualizar empregado (por id)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListaEmpregadosComponent } from './lista-empregados/lista-empregados.component';
 import { CadastrarEmpregadoComponent } from './cadastrar-empregado/cadastrar-empregado.component';
 import { BuscarEmpregadoComponent } from './buscar-empregado/buscar-empregado.component';
+import { AtualizarEmpregadoComponent } from './atualizar-empregado/atualizar-empregado.component';
 
 const routes: Routes = [
   {path: 'empregados', component: ListaEmpregadosComponent},
   {path: '', redirectTo: 'empregados', pathMatch: 'full'},
   {path: 'cadastrar-empregado', component: CadastrarEmpregadoComponent},
-  {path: 'buscar-empregado-id/:id', component: BuscarEmpregadoComponent}
+  {path: 'buscar-empregado-id/:id', component: BuscarEmpregadoComponent},
+  {path: 'atualizar-empregado/:id', component: AtualizarEmpregadoComponent}
 ];
 
 @NgModule({
